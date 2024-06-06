3f237315c995012f31324815f908f4f22de9cef7
a84801 <149081686+a84801@users.noreply.github.com>
2024-06-06 19:43:22 +0100

    Alteração do servidor em Flask

--- servidor.py
+++ servidor.py
@@ -47,3 +47,42 @@ def delete_cliente(id):
     conn.commit()
     conn.close()
     return 'Cliente excluído com sucesso.'
+
+# Serve para obter todos os dados na tabela "Users"
+@app.route('/users', methods=['GET'])
+def get_user():
+    conn = db_connection()
+    user = conn.execute('SELECT * FROM Users').fetchall()
+    conn.close()
+    return jsonify([{'email': row[0], 'password': row[1]} for row in user])
+
+# Serve para adicionar dados na tabela "Users"
+@app.route('/users', methods=['POST'])
+def add_user():
+    new_user = request.json
+    conn = db_connection()
+    conn.execute('INSERT INTO Users (email, password) VALUES (?, ?)',
+                 (new_user['email'], new_user['password']))
+    conn.commit()
+    conn.close()
+    return 'Novo user adicionado com sucesso.', 201
+
+# Serve para atualizar dados na tabela de dados "Users"
+@app.route('/users/<int:id>', methods=['PUT'])
+def update_user(id):
+    updated_user = request.json
+    conn = db_connection()
+    conn.execute('UPDATE Users SET email=?, password=? WHERE id=?',
+                 (updated_user['email'], updated_user['password'], id))
+    conn.commit()
+    conn.close()
+    return 'User atualizado com sucesso.'
+
+# Serve para excluir dados na tabela de dados "Users"
+@app.route('/users/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    conn = db_connection()
+    conn.execute('DELETE FROM Users WHERE id=?', (id,))
+    conn.commit()
+    conn.close()
+    return 'User excluído com sucesso.'
